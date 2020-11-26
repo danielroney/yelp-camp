6b843ee00ff59cae5e8ec98bcd7c5b36be6970d2
Dan Roney <64231132+danielroney@users.noreply.github.com>
2020-11-26 14:09:45 -0800

    add image and price to model and show page

--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,10 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/289662',
+            description: 'Lucas ipsum dolor sit amet hutta kashyyyk brak gonk nelvaanian metalorn mimbanite solo mirax askajian. Klivian thrackan dengar utapau maul lando luke lando kenobi. Fisto hutt var moff. Amidala ybith solo sebulba chiss jagged. Raioballo rune vodran barabel cathar sikan dantari ssi-ruuk bothan. Maris shmi moff tatooine vratix vurk carnor maximilian. Ponda cabasshite nadon rugor var mirta balosar. Hapan vratix rugor aruzan. Baba daala sabé lumiya spar ubb leia moor. Ors kowakian metalorn annoo bando cliegg sidious tiin.',
+            price: 30
         })
         await camp.save();
     }
