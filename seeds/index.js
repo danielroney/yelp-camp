b40df55dec6fce4b06c59fcb3c8f9831839bd4e9
Dan Roney <64231132+danielroney@users.noreply.github.com>
2021-01-01 09:24:59 -0800

    temp seeds change

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,7 @@ const seedDB = async () => {
     for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            author: '5fd8d0f511167e44fcdbe136',
+            author: '5fef573112943d001781afb4',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             geometry: {
                 type: 'Point',
